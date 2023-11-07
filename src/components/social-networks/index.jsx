239281ac18c148b6fa11b1f1014fb19739fcadc0
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2023-11-07 15:45:09 +0100

    Creating a component for social networks

---
+++ src/components/social-networks/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+import github from '../../../public/assets/social_network_icons/github.png'
+import linkedin from '../../../public/assets/social_network_icons/linkedin.png'
+import Link from 'next/link'
+
+
+export default function SocialNetworks() {
+  return (
+    <div className='flex gap-5'>
+      <Link href="https://github.com/Jimmy-Ctln" target='_blank'>
+        <Image className='w-14 h-14 cursor-pointer' src={github} alt='logo github'/>
+      </Link>
+      <Link href="https://www.linkedin.com/in/jimmycatalano/" target='_blank'>
+        <Image className='w-14 h-14 cursor-pointer' src={linkedin} alt='logo linkedin'/>
+      </Link>
+    </div>
+  )
+}
