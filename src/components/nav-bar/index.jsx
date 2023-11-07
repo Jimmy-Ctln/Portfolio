7e9675637bc8c08872226ebcfca380e30e23eece
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2023-11-07 15:46:10 +0100

    Code overhaul

--- src/components/nav-bar/index.jsx
+++ src/components/nav-bar/index.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <nav className=''>
+    <nav>
         <ul className='flex justify-end gap-10'> 
-            <li>Home</li>
-            <li>Qui suis-je ?</li>
-            <li>Compétences</li>
-            <li>Expériences</li>
-            <li>Réalisation</li>
-            <li>Contact</li>
+            <li className='cursor-pointer'>Home</li>
+            <li className='cursor-pointer'>Qui suis-je ?</li>
+            <li className='cursor-pointer'>Compétences</li>
+            <li className='cursor-pointer'>Expériences</li>
+            <li className='cursor-pointer'>Réalisation</li>
+            <li className='cursor-pointer'>Contact</li>
         </ul>  
     </nav>
   )
