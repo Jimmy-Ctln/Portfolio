7d69d5f0c08509f8002acea3dfab3cd51000b334
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2023-11-10 18:28:44 +0100

    code refactoring

--- src/app/sections/skills/index.jsx
+++ src/app/sections/skills/index.jsx
@@ -1,19 +1,27 @@
 import React from "react";
+import Image from "next/image";
 import { Languages } from "../../../components/languages";
 import data from "../../../data/data.json";
+import frame9 from '../../../../public/assets/diamond_frames/frame9.png'
+import frame10 from '../../../../public/assets/diamond_frames/frame10.png'
 
 export const Skills = () => {
-  let coucou = "coucou";
-  console.log(coucou);
+
   return (
-    <div>
-        <div className="text-center">
-          <h3 className="text-4xl font-bold">COMPÉTENCES</h3>
+    <div id="skills" className="w-full relative">
+      <div className=" absolute left-686px top-600px rotate-[-90deg]">
+        <Image src={frame10} alt="diamond frame"/>
+      </div>
+        <div className="flex justify-center items-center">
+          <h3 className="text-4xl h-auto font-bold">COMPÉTENCES</h3>
+          <div className="w-48 h-48 relative bottom-0">
+            <Image src={frame9} alt="diamond frame"/>
+          </div>
         </div>
-      <div className="flex flex-col gap-10 w-1/2">
-        <div className="flex flex-col ml-40 gap-5 w-full">
+      <div className="flex flex-col items-center gap-20 mt-20">
+        <div className="flex flex-col gap-5 relative right-64">
           <h4>INTÉGRATION</h4>
-          <div className="flex gap-10 h-auto rounded-2xl justify-center p-2 bg-rectangle p">
+          <div className="flex gap-8 w-96 h-auto rounded-2xl justify-center py-3.5 px-3  bg-rectangle p">
             {data.languages.integration.map((language) => (
               <Languages
                 key={language.id}
@@ -23,9 +31,9 @@ export const Skills = () => {
             ))}
           </div>
         </div>
-        <div className="flex flex-col gap-5 ml-96 w-full">
+        <div className="flex flex-col gap-5 relative left-64">
           <h4>FRONTEND</h4>
-          <div className="flex gap-10 h-auto rounded-2xl justify-center p-2 bg-rectangle p">
+          <div className="flex gap-8 w-96 h-auto rounded-2xl justify-center py-3.5 px-3 bg-rectangle p">
             {data.languages.frontend.map((language) => (
               <Languages
                 key={language.id}
@@ -33,6 +41,14 @@ export const Skills = () => {
                 src={language.src}
               />
             ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-5 relative right-64">
+          <h4>BACKEND</h4>
+          <div className="flex gap-8 w-96 h-102.78 rounded-2xl justify-center py-3.5 px-3 bg-rectangle p">
+            <div className="flex items-center">
+              <p>EN COURS...</p>
+            </div>
           </div>
         </div>
       </div>
