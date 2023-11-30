f5b0afd9c11db4521bfb52246c82bda9c91e6744
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2023-11-30 01:35:37 +0100

    Editing the carousel + responsive

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -5,10 +5,7 @@ import Image from "next/image";
 import { useState } from "react";
 import arrowLeft from "../../../public/assets/icons/arrow-left.png";
 import arrowRight from "../../../public/assets/icons/arrow-right.png";
-import github from "../../../public/assets/icons/github.png";
-import link from "../../../public/assets/icons/link.png";
 import data from "../../data/data.json";
-import Link from "next/link";
 
 export const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -32,16 +29,15 @@ export const Carousel = () => {
   };
 
   return (
-    <div className="relative">
       <div className="flex justify-center mt-10">
-        <div className="flex items-center gap-5 justify-center">
+        <div className="flex items-center gap-5 justify-center w-11/12 px-1">
           <Image
             src={arrowLeft}
-            className="w-20 h-auto cursor-pointer"
+            className="w-20 h-auto cursor-pointer max-[945px]:w-11 max-sm:absolute max-sm:z-50 left-0"
             onClick={handlePrev}
             alt="arrow left button"
           />
-          <div className="w-96 h-420 blur-sm mr-[-60px] brightness-50">
+          <div className="w-96 h-420 blur-sm mr-[-60px] brightness-50 max-xl:w-[200px] max-xl:h-[200px] max-[945px]:hidden">
             <Image
               key={prevProject.id}
               src={prevProject.src}
@@ -57,34 +53,14 @@ export const Carousel = () => {
               alt={`project ${prevProject.name}`}
             />
           </div>
-          <div className="w-700 h-461 z-10 relative">
-            <div className="before-element absolute rounded-bl-[20px] rounded-br-[20px] bottom-0 w-full h-20 bg-secondary opacity-100">
+          <div className="w-700 h-461 z-10 relative max-xl:w-[600px] max-xl:h-[400px] max-sm:w-full max-sm:h-[300px]">
+            <div className="before-element absolute rounded-bl-[20px] rounded-br-[20px] bottom-0 w-full h-20 bg-secondary opacity-100 max-lg:h-24">
               <div className="absolute bottom-0 flex justify-center items-center w-full h-full gap-14">
-                <div className="flex gap-20 items-center">
-                  <p className="font-bold text-base">{currentProject.name}</p>
-                  <p className="w-72 text-sm">{currentProject.description}</p>
+                <div className="flex gap-20 items-center max-xl:gap-[2%] max-lg:justify-around">
+                  <p className="font-bold text-base max-lg:w-1/4 max-lg:text-sm">{currentProject.name}</p>
+                  <p className="w-72 text-sm max-lg:w-1/2 max-lg:text-xs">{currentProject.description}</p>
                 </div>
               </div>
-                {/* <div className="absolute right-[25px] bottom-[25px] flex items-center gap-5">
-                  <Link href={currentProject.websiteLink}>
-                    <Image
-                      src={link}
-                      width={30}
-                      height={30}
-                      className="cursor-pointer"
-                      alt="link to the online project"
-                    />
-                  </Link>
-                  <Link href={currentProject.githubLink}>
-                    <Image
-                      src={github}
-                      className="cursor-pointer"
-                      width={30}
-                      height={30}
-                      alt="project link on github"
-                    />
-                  </Link>
-                </div> */}
             </div>
             <Image
               key={currentProject.id}
@@ -101,7 +77,7 @@ export const Carousel = () => {
               alt={`project ${currentProject.name}`}
             />
           </div>
-          <div className="w-96 h-420 blur-sm ml-[-60px] brightness-50">
+          <div className="w-96 h-420 blur-sm ml-[-60px] brightness-50 max-xl:w-[200px] max-xl:h-[200px] max-[945px]:hidden">
             <Image
               key={nextProject.id}
               src={nextProject.src}
@@ -119,12 +95,11 @@ export const Carousel = () => {
           </div>
           <Image
             src={arrowRight}
-            className="w-20 h-auto cursor-pointer"
+            className="w-20 h-auto cursor-pointer max-[945px]:w-11 max-sm:absolute max-sm:z-50 right-0"
             onClick={handleNext}
             alt="arrow right button"
           />
         </div>
       </div>
-    </div>
   );
 };
