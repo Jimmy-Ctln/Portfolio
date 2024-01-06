bd2befdd2169dd33edc119b6bd107493ccbcc5fa
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-01-06 14:32:52 +0100

    Changing images

--- src/components/languages/index.jsx
+++ src/components/languages/index.jsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export const Languages = ({ src, name }) => {
   return (
-    <div className="flex flex-col gap-1 items-center justify-center w-full rounded-xl">
+    <div className="flex flex-col items-center justify-center w-full rounded-xl">
       <Image
         src={src}
         width={100}
         height={100}
-        className="w-[50px] h-auto"
+        className="w-[50px] min-h-[80px]"
         alt={name}
       />
       <div>
